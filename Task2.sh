72bb74a6607c1fd8db284abadccc3b2a124185f4
Johnsilenin-14 <76509664+Johnsilenin-14@users.noreply.github.com>
2024-02-18 14:55:45 +0530

    Update Task2.sh

--- Task2.sh
+++ Task2.sh
@@ -6,13 +6,13 @@ num_sequences=$(zgrep -c '^>' NC_000913.faa.gz)
 #count the total  number of amino acids
 num_aminoacids=$(zgrep -v '^>' NC_000913.faa.gz | tr -d '\n[:space:]'|wc -c)
 
-# Calculate the average number of amino acids per sequence
-average_aa_per_sequence=$(echo "$num_aminoacids / $num_sequences" | bc)
+# Calculate the average length
+average=$(echo "$num_aminoacids / $num_sequences" | bc)
 
 # Print the results
 echo "Number of sequences: $num_sequences"
 echo "Total number of amino acids: $num_aminoacids"
-echo "Average length of the protein : $average_aa_per_sequence"
+echo "Average length of the protein : $average"
 
 
 ###Single line command##
